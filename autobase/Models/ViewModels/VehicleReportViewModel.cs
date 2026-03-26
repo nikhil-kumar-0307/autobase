08cad80855b79816b6a229423c8490288c9d082f
Nikhil Kumar <nikhilgupta03067@gmail.com>
2026-03-26 14:55:42 +0530

    Vehicle Report Chart
    1. Implemented the logic to show the graph representation of the vehicle used and unused vehicle.
    2. Graph is visible on the monthly and weekly basis.

---
+++ autobase/Models/ViewModels/VehicleReportViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace autobase.Models.ViewModels
+{
+    public class VehicleReportViewModel
+    {
+        public string Period { get; set; } = "week";
+
+        public int TotalRequests { get; set; }
+        public string MostUsedVehicle { get; set; }
+        public int MostUsedTrips { get; set; }
+        public string MostAvailableVehicle { get; set; }
+        public int MostAvailableDays { get; set; }
+        public int UtilisationPercent { get; set; }
+
+        public List<string> VehicleNames { get; set; } = new();
+        public List<int> UsedTrips { get; set; } = new();
+        public List<int> AvailableDays { get; set; } = new();
+        public List<string> TrendLabels { get; set; } = new();
+        public List<VehicleTrendLine> TrendLines { get; set; } = new();
+
+        public int StatusAvailable { get; set; }
+        public int StatusInUse { get; set; }
+        public int StatusMaintenance { get; set; }
+    }
+
+    public class VehicleTrendLine
+    {
+        public string VehicleName { get; set; }
+        public List<int> Data { get; set; } = new();
+    }
+}
